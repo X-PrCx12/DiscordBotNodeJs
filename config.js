3393db77abcb339038cd2cd460a7bdcb820c77d7
PUDIDI <74692006+X-PrCx12@users.noreply.github.com>
2021-02-10 21:52:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.config = {
     // bot owner discord id
-    OWNER_ID: "101041126537973760",
+    OWNER_ID: "806738477713719306",
 
     // bot prefix
     PREFIX: ".",
@@ -10,7 +10,7 @@ exports.config = {
 
     // where to send welcome message (guildID : channelID)
     WELCOME_MESSAGE_CHANNEL: {
-        "408735794094669824": "408737565298917397",
+        "807465036137758742": "408737565298917397",
         "617836255350358037": "617836256268779521"  // dog
         //"144518146232025088": "193151740743385089"
     }
